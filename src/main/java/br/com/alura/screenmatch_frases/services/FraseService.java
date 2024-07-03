13de4a6ab000b84b12ee8c6d79661bfea3da260f
Luis Fernando Cutilak <luiscutilak@gmail.com>
2024-07-03 08:21:05 -0300

    feat: desafio concluido, utilizando arquitetura de software, sob camadas

--- src/main/java/br/com/alura/screenmatch_frases/services/FraseService.java
+++ src/main/java/br/com/alura/screenmatch_frases/services/FraseService.java
@@ -1,13 +1,18 @@
 package br.com.alura.screenmatch_frases.services;
 
 import br.com.alura.screenmatch_frases.dto.FraseDTO;
+import br.com.alura.screenmatch_frases.model.Frase;
+import br.com.alura.screenmatch_frases.repository.FraseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class FraseService {
     @Autowired
     private FraseRepository repositorio;
 
     public FraseDTO obterFraseAleatoria() {
-        return repositorio.
+        Frase frase = repositorio.buscaFraseAleatoria();
+        return new FraseDTO(frase.getTitulo(), frase.getFrase(), frase.getPersonagem(), frase.getPoster());
     }
 }
